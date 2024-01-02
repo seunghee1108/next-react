05feaa544710ce94d10f19baac2c8644d4454d01
승희 <bangseunghee1108@gmail.com>
2024-01-02 15:43:10 +0900

    input에 이름 입력하면 일치하는 멤버를 찾아 해당 멤버의 페이지로 이동

--- practice/app/page.tsx
+++ practice/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
+
 import styles from '/app/page.module.css'
 import { useState } from 'react';  // CSS 모듈(page.module.css)을 사용하여 컴포넌트에 스타일을 적용 
-import React, { useRef } from 'react';
+import React, { useRef } from 'react'; // React 라이브러리에서 React 객체와 useRef 훅을 불러옴
 
 
-// 'Members'라는 인터페이스를 정의한다.
-// 각 멤버의 정보를 담는 객체의 형태를 정의한 것이다.
+// 'Members'라는 인터페이스를 정의함
+// 각 멤버의 정보를 담는 객체의 형태를 정의한 것임
 interface Members{
   hash:string;
   name:string;
@@ -44,63 +45,68 @@ const memberData:Members[] = [
   { hash: '홍문기', name: '홍문기', content: '홍문기 소개' , contentTwo: contentText , contentThree: '홍문기 장점', contentFour: contentTextTwo },
 ];
 
-
 export default function Home() {
-  // useState 훅을 이용한 상태 관리
-  // section 이라는 상태 변수와 그 상태를 업데이트할 수 있는 useSection 함수를 생성한다. 
   const [section, setSection] = useState('');
+  const inputRef = useRef<HTMLInputElement | null>(null); // useRef를 사용하여 inputRef 생성
 
-  // 멤버의 이름이 클릭되었을 때 호출되는 함수이다.
-  // 선택된 멤버의 'hash'값을 'section' 상태로 업데이트 하고, 현재 페이지의 해시 값을 변경한다.
-  const nameClick = (targetSection:string) => {
+  const nameClick = (targetSection: string) => {
     setSection(targetSection);
     window.location.hash = `#${targetSection}`;
   };
 
-  
-  // 페이지의 메인 컴포넌트를 정의한다.
-  // 이 안에서 멤버 목록('ul')과 멤버 소개 내용('div')이 표시된다.
+  const handleSearch = () => {
+    if (inputRef.current) {
+      const inputName = inputRef.current.value;
+
+      const matchingMember = memberData.find((member) => member.name === inputName);
+
+      if (matchingMember) {
+        setSection(matchingMember.hash);
+        window.location.hash = `#${matchingMember.hash}`;
+      }
+    }
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.head}>
         <ul>
-          {/* 멤버 목록 출력 : 멤버 데이터를 매핑하여 각 멤버의 이름을 목록으로 출력하고, 클릭시 'nameClick' 함수를 호출한다. */}
-        {memberData.map(({ hash, name }) => (
-          <li key={hash} onClick={() => nameClick(hash)}>{name}</li>
+          {memberData.map(({ hash, name }) => (
+            <li key={hash} onClick={() => nameClick(hash)}>
+              {name}
+            </li>
           ))}
         </ul>
       </div>
       <div className={styles.board}>
-        {/* 멤버 소개 내용 출력 : 멤버 데이터를 매핑하여 각 멤버의 소개 내용을 출력하고, 현재 선택된 멤버에 해당하는 내용만 보여주도록 스타일을 제어한다. */}
-      {memberData.map(({ hash, content }) => (
-        <div key={hash} id={hash} style={{ display: section === hash ? 'block' : 'none' }}>
+        {memberData.map(({ hash, content }) => (
+          <div key={hash} id={hash} style={{ display: section === hash ? 'block' : 'none' }}>
             {content}
           </div>
         ))}
-      {memberData.map(({ hash, contentTwo })=> (
-          <div key={hash} id={hash}  style={{ display: section === hash ? 'block' : 'none' }}>
+        {memberData.map(({ hash, contentTwo }) => (
+          <div key={hash} id={hash} style={{ display: section === hash ? 'block' : 'none' }}>
             {contentTwo}
           </div>
         ))}
-      {memberData.map(({ hash, contentThree })=> (
+        {memberData.map(({ hash, contentThree }) => (
           <div key={hash} id={hash} style={{ display: section === hash ? 'block' : 'none' }}>
             {contentThree}
           </div>
         ))}
-      {memberData.map(({ hash, contentFour })=> (
+        {memberData.map(({ hash, contentFour }) => (
           <div key={hash} id={hash} style={{ display: section === hash ? 'block' : 'none' }}>
             {contentFour}
           </div>
         ))}
         <div>
           <label htmlFor="customInput"></label>
-          <input type="text" name="customInput" placeholder='Write your name.'/>
+          <input type="text" name="customInput" ref={inputRef} placeholder="Write your name." />
         </div>
-        {/* <button id="btn">Show</button> */}
         <div>
-          <button >Show</button>
+          <button onClick={handleSearch}>Show</button>
         </div>
-        </div>
+      </div>
     </main>
-  )
+  );
 }
